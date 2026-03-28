ad5e1e1f1aa16095ab866254f096ffbb9ba8994b
Scoogend <girot.hugo@yahoo.com>
2026-03-28 21:49:46 +0000

    Initial commit - Generated by Visual Nebula Creator

---
+++ app/assets/js/distromanager.js
@@ -0,0 +1,17 @@
+const { DistributionAPI } = require('helios-core/common')
+
+const ConfigManager = require('./configmanager')
+
+// Old WesterosCraft url.
+// exports.REMOTE_DISTRO_URL = 'https://pub-e48eb57ae84146dea084aafca4dc9655.r2.dev/public/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+
+const api = new DistributionAPI(
+    ConfigManager.getLauncherDirectory(),
+    null, // Injected forcefully by the preloader.
+    null, // Injected forcefully by the preloader.
+    exports.REMOTE_DISTRO_URL,
+    false
+)
+
+exports.DistroAPI = api
